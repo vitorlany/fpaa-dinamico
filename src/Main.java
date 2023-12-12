ec904474705d344025d34245ee1be4ab2e732805
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2023-12-11 21:27:19 -0300

    formato relatório

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,34 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<int[]> rotasGeradas = GeradorDeProblemas.geracaoDeRotas(25, 1, 1);
-        int caminhoes = 3;
+        List<int[]> rotasGeradas = GeradorDeProblemas.geracaoDeRotas(25, 10, 1);
+        final int caminhoes = 3;
 
-        List<Integer> set = new ArrayList<>(Arrays.stream(rotasGeradas.get(0))
+        List<List<DtoResposta>> respostas = new ArrayList<>();
+
+        for (int[] rotasGerada : rotasGeradas) {
+            respostas.add(getRotas(caminhoes, rotasGerada));
+        }
+
+        System.out.println(respostas);
+    }
+
+    private static List<DtoResposta> getRotas(int caminhoes, int[] array) {
+        List<Integer> set = new ArrayList<>(Arrays.stream(array)
                 .boxed()
                 .toList());
-
-        // Calcula o ideal para cada um, e arredonda, evitando decimais
-        int sum = (int) Math.ceil(set.stream().mapToDouble(a -> a).sum()/caminhoes);
-
-        System.out.println("Initial: " + set);
+        List<DtoResposta> respostas = new ArrayList<>();
+        int sum = (int) Math.ceil(set.stream().mapToDouble(a -> a).sum()/ caminhoes);
 
         for (int i = 0; i < caminhoes; i++) {
-            System.out.println("Caminhão " + (i+1));
+            String label = "Caminhão " + (i + 1);
+
+            long timmer = System.currentTimeMillis();
             boolean[][] subsetSum = GFG.subsetSum(set, sum);
             Rota rota = GFG.getResults(set, subsetSum);
-            System.out.println(rota.rotas());
-            System.out.println(rota.total());
+            timmer = System.currentTimeMillis() - timmer;
+
+            respostas.add(new DtoResposta(label, rota, timmer));
 
             // Garantimos que removemos apenas 1
             for (int r: rota.rotas()) {
@@ -31,7 +41,7 @@ public class Main {
 
             // Garante margem de erro que não foi suprida
             sum += sum - rota.total();
-            System.out.println("====================");
         }
+        return respostas;
     }
 }
